be1b59dd649327e025e93fba4cd05ec7bd10c1d4
LAPTA <douaasg20@gmail.com>
2026-05-07 13:55:21 +0100

    feat: add initial pricing engine with bad design

--- pricing-engine/app/src/main/java/org/example/PricingEngine.java
+++ pricing-engine/app/src/main/java/org/example/PricingEngine.java
@@ -1,28 +1,34 @@
 package org.example;
 
 public class PricingEngine {
+    public static void main(String[] args) {
+        double[] prices = {100, 50, 25};
+        int[] quantities = {2, 1, 4};
+        String customerType = "VIP";
+        String discountCode = "SAVE10";
 
-    public double calculateFinalPrice(double subtotal,
-                                      String customerType,
-                                      String discountCode) {
+        double subtotal = 0;
+        for (int i = 0; i < prices.length; i++) {
+            subtotal += prices[i] * quantities[i];
+        }
 
         double discount = 0;
+        if (discountCode.equals("SAVE10")) {
+            discount = subtotal * 0.10;
+        } else if (discountCode.equals("SAVE20")) {
+            discount = subtotal * 0.20;
+        }
 
-        // VIP discount
         if (customerType.equals("VIP")) {
-            discount += subtotal * 0.10;
+            discount += subtotal * 0.05;
         }
 
-        // Discount code
-        if (discountCode.equals("SAVE20")) {
-            discount += subtotal * 0.20;
-        }
+        double tax = (subtotal - discount) * 0.19;
+        double finalPrice = subtotal - discount + tax;
 
-        double afterDiscount = subtotal - discount;
-
-        // Tax
-        double tax = afterDiscount * 0.07;
-
-        return afterDiscount + tax;
+        System.out.println("Subtotal: " + subtotal);
+        System.out.println("Discount: " + discount);
+        System.out.println("Tax: " + tax);
+        System.out.println("Final Price: " + finalPrice);
     }
 }
